a09b55eb6291b0c46835cbbf5c03c28048da10f9
Nishikant Parmar <nishikantparmar123@gmail.com>
2021-03-26 16:42:08 +0530

    Scrapped csranking-0.csv

--- scraping/scrape_data.py
+++ scraping/scrape_data.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import pprint
-from urllib.request import HTTPCookieProcessor, Request, build_opener
-from http.cookiejar import MozillaCookieJar
+import csv
+import time
+import random
 
-pp = pprint.PrettyPrinter(indent=4)
+
+
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
 
 def extract_paper_info(paper_url):
@@ -23,7 +24,7 @@ def extract_paper_info(paper_url):
     page = requests.get(paper_url,headers=headers)    
 
     if page.status_code == 404:
-        return (False)
+        return (False,'', '', '', '', '', '', '', [])
 
     soup = BeautifulSoup(page.content, 'html.parser') 
 
@@ -84,51 +85,70 @@ def extract_prof_info(scholar_id):
     url_base = 'https://scholar.google.com/citations?user='
     url = url_base + scholar_id
     page = requests.get(url,headers=headers)
+    time.sleep(random.randint(1,3))
 
     # if scholar page does not exist
     if page.status_code == 404:
-        return (False)    
+        return (False, '', '', '', '', '', [], 0, 0, 0, 0, 0, 0, [], '', [], [])    
 
-    soup = BeautifulSoup(page.content, 'html.parser')    
-    right_box = soup.find_all("td", class_ = 'gsc_rsb_std')    
-    cit = int(right_box[0].string)
-    h_ind = int(right_box[2].string)
-    i_ind = int(right_box[4].string)
-    cit5 = int(right_box[1].string)
-    h_ind5 = int(right_box[3].string)
-    i_ind5 = int(right_box[5].string)
-    inf = soup.find_all("div", class_ = 'gsc_prf_il')
-    topics = inf[2].find_all('a')
-    topics_list = [topic.text for topic in topics]
-    email = (inf[1].text[18:-11])
-    affiliation = inf[0].text
-    homepage = soup.find_all('a', class_ = 'gsc_prf_ila')[-1]['href']
-    name = soup.find("div", {"id": "gsc_prf_in"}).text
-    cit_list = [int(cit.text) for cit in soup.find_all('span', class_ = 'gsc_g_al')]
-    image_url = soup.find("img", {"id": "gsc_prf_pup-img"})['src']
-    if image_url == '/citations/images/avatar_scholar_128.png':
-        image_url = 'https://scholar.google.co.in/citations/images/avatar_scholar_128.png'
-    papers_url_list = ['https://scholar.google.co.in' + paper['data-href'] for paper in soup.find_all('a', class_ = 'gsc_a_at')]
-    papers_title_list = [paper.contents[0] for paper in soup.find_all('a', class_ = 'gsc_a_at')]
+    try:
+        soup = BeautifulSoup(page.content, 'html.parser')            
+        right_box = soup.find_all("td", class_ = 'gsc_rsb_std')    
+        cit = int(right_box[0].string)
+        h_ind = int(right_box[2].string)
+        i_ind = int(right_box[4].string)
+        cit5 = int(right_box[1].string)
+        h_ind5 = int(right_box[3].string)
+        i_ind5 = int(right_box[5].string)
+        inf = soup.find_all("div", class_ = 'gsc_prf_il')
+        topics = inf[2].find_all('a')
+        topics_list = [topic.text for topic in topics]
+        email = (inf[1].text[18:-11])
+        affiliation = inf[0].text
+        homepage = soup.find_all('a', class_ = 'gsc_prf_ila')[-1]['href']
+        name = soup.find("div", {"id": "gsc_prf_in"}).text
+        cit_list = [int(cit.text) for cit in soup.find_all('span', class_ = 'gsc_g_al')]
+        image_url = soup.find("img", {"id": "gsc_prf_pup-img"})['src']
+        if image_url == '/citations/images/avatar_scholar_128.png':
+            image_url = 'https://scholar.google.co.in/citations/images/avatar_scholar_128.png'
+        papers_url_list = ['https://scholar.google.co.in' + paper['data-href'] for paper in soup.find_all('a', class_ = 'gsc_a_at')]
+        papers_title_list = [paper.contents[0] for paper in soup.find_all('a', class_ = 'gsc_a_at')]
+    except:
+        return (False, '', '', '', '', '', [], 0, 0, 0, 0, 0, 0, [], '', [], [])        
 
     return (True, name, image_url, affiliation, email, homepage, topics_list, cit, h_ind, i_ind, cit5, h_ind5, i_ind5, cit_list, image_url, papers_url_list, papers_title_list)
 
+def write_prof_data_to_csv(professor_file, professor_data_to_write):
+
+    try:
+        write = csv.writer(professor_file)
+        write.writerow(professor_data_to_write)
+    except:
+        pass
+
 df = pd.read_csv('csrankings-0.csv')
 
-professor_info = dict()
 papers_info = dict()
 
-for i in range(1):
+professor_file = open('professor_data.csv', 'a+',newline ='')
 
-    scholar_id = df.iloc[i]['scholarid']    
+for i in range(158, len(df)):
+
+    scholar_id = df.iloc[i]['scholarid'] 
+
+    if scholar_id == 'NOSCHOLARPAGE':
+        continue
+
     found, name, image_url, affiliation, email, homepage, topics_list, cit, h_ind, i_ind, cit5, h_ind5, i_ind5, cit_list, image_url, papers_url_list, papers_title_list = extract_prof_info(scholar_id)
 
     if not found:
         continue
 
-    professor_info[scholar_id] = (name, image_url, affiliation, email, homepage, topics_list, cit, h_ind, i_ind, cit5, h_ind5, i_ind5, cit_list, image_url, papers_url_list, papers_title_list)    
+    professor_data_to_write = (scholar_id, name, image_url, affiliation, email, homepage, topics_list, cit, h_ind, i_ind, cit5, h_ind5, i_ind5, cit_list, image_url, papers_url_list, papers_title_list)    
+    write_prof_data_to_csv(professor_file, professor_data_to_write)
+    
 
-    for j in range(min(len(papers_url_list), 2)):
+    for j in range(min(len(papers_url_list), 0)):
 
         paper_url = papers_url_list[j]
         title = papers_title_list[j]
@@ -141,13 +161,3 @@ for i in range(1):
             papers_info[title][9].append(scholar_id)        
         else:
             papers_info[title] = (paper_url, original_url, description, publisher, conference, publication_date, authors, journal, cit_list, [scholar_id])
-
-
-print(professor_info)
-print("\n")
-print(papers_info)
-
-# req = Request(url='https://scholar.google.com/citations?user=ZLpO3XQAAAAJ&hl=en', headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0'})
-# opener  = build_opener(HTTPCookieProcessor(MozillaCookieJar()))
-# hdl = opener.open(req)
-# html = hdl.read()
