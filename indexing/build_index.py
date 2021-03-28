acbe3464e5191b16f81152e007e4a708000c5239
Nishikant Parmar <nishikantparmar123@gmail.com>
2021-03-28 18:54:04 +0530

    Built index for research topics

--- indexing/build_index.py
+++ indexing/build_index.py
@@ -2,13 +2,14 @@ import csv
 import pandas as pd
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
+import sys
 
-try:
-    input_file = pd.read_csv('../../professor_data_combined.csv',header=None)
-    output_file = open('research_topic_index.csv','w',newline='',errors="ignore")
+try:    
+    output_file = open('research_topic_index.csv','w',newline='',errors="ignore",encoding='latin1')
     output_writer = csv.writer(output_file)
 except:
-    print("Error in input and/or output file.")
+    print("Error in opening output file.")
+    sys.exit(0)
 
 
 research_topic_index = dict()
@@ -39,27 +40,36 @@ def make_list_citations(initial_string):
     except:
         return []
 
-for i in range(len(input_file)):    
+file_count = 10
 
-    scholar_id = input_file.iloc[i][0]
-    name = input_file.iloc[i][1]
-    image_url = input_file.iloc[i][2]
-    affiliation = input_file.iloc[i][3]
-    email = input_file.iloc[i][4]
-    homepage = input_file.iloc[i][5]
-    topics_list = make_list(input_file.iloc[i][6])
-    cit = int(input_file.iloc[i][7])
-    h_ind = int(input_file.iloc[i][8])
-    i_ind = int(input_file.iloc[i][9])
-    cit5 = int(input_file.iloc[i][10])
-    h_ind5 = int(input_file.iloc[i][11])
-    i_ind5 = int(input_file.iloc[i][12])    
-    cit_list = make_list_citations(input_file.iloc[i][13])
-    image_url = input_file.iloc[i][14]
-    papers_url_list = make_list(input_file.iloc[i][15])
-    papers_title_list = make_list(input_file.iloc[i][16])    
+for file_index in range(file_count):              
+    try:
+        input_file = pd.read_csv('../scraping/professor_data-'+str(file_index)+'.csv',header=None,encoding='latin1')
+    except:
+        print("Error in opening input file.")
+        sys.exit(0)
 
-    build_index_for_research_topic(topics_list, scholar_id)        
+    for i in range(len(input_file)):    
+
+        scholar_id = input_file.iloc[i][0]
+        name = input_file.iloc[i][1]
+        image_url = input_file.iloc[i][2]
+        affiliation = input_file.iloc[i][3]
+        email = input_file.iloc[i][4]
+        homepage = input_file.iloc[i][5]
+        topics_list = make_list(input_file.iloc[i][6])
+        cit = int(input_file.iloc[i][7])
+        h_ind = int(input_file.iloc[i][8])
+        i_ind = int(input_file.iloc[i][9])
+        cit5 = int(input_file.iloc[i][10])
+        h_ind5 = int(input_file.iloc[i][11])
+        i_ind5 = int(input_file.iloc[i][12])    
+        cit_list = make_list_citations(input_file.iloc[i][13])
+        image_url = input_file.iloc[i][14]
+        papers_url_list = make_list(input_file.iloc[i][15])
+        papers_title_list = make_list(input_file.iloc[i][16])    
+
+        build_index_for_research_topic(topics_list, scholar_id)        
 
 count = 0
 sum_len = 0
