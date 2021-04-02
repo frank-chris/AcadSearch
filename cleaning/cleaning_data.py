67c6ce9d6824e676efae3be328b180952eaff1c5
Nishikant Parmar <nishikantparmar123@gmail.com>
2021-04-02 16:45:36 +0530

    Cleaned data

--- cleaning/cleaning_data.py
+++ cleaning/cleaning_data.py
@@ -9,30 +9,36 @@ def write_prof_data_to_csv(output_file, professor_data_to_write):
     try:
         write = csv.writer(output_file)
         write.writerow(professor_data_to_write)
-    except:
+    except:        
         pass
+
+def check_for_nan(input_string):
+    if pd.isna(input_string):
+        return ''
+    else:
+        return input_string
 
 for file_index in range(file_count):    
 
     try:
-        input_file = pd.read_csv('../scraping/professor_data-'+str(file_index)+'.csv',header=None,encoding='latin1')
-        output_file = open('professor_data-'+str(file_index)+'.csv', 'a+',newline ='')
+        input_file = pd.read_csv('../scraping/professor_data-'+str(file_index)+'.csv',header=None,encoding='utf8')
+        output_file = open('professor_data-'+str(file_index)+'-cleaned.csv', 'w+',newline ='',encoding='utf8')
     except:
         print("Error in opening input/output file.")
         sys.exit(0)
 
-    number_of_professors = len(input_file)
+    number_of_professors = len(input_file)    
 
-    previous_scholar_id = None
+    scholar_ids = set()    
     
     for prof_index in range(number_of_professors):    
 
         scholar_id = input_file.iloc[prof_index][0]
-        name = input_file.iloc[prof_index][1]
+        name = check_for_nan(input_file.iloc[prof_index][1])
         image_url = input_file.iloc[prof_index][2]
-        affiliation = input_file.iloc[prof_index][3]
-        email = input_file.iloc[prof_index][4]
-        homepage = input_file.iloc[prof_index][5]
+        affiliation = check_for_nan(input_file.iloc[prof_index][3])
+        email = check_for_nan(input_file.iloc[prof_index][4])
+        homepage = check_for_nan(input_file.iloc[prof_index][5])
         topics_list = input_file.iloc[prof_index][6]
         cit = input_file.iloc[prof_index][7]
         h_ind = input_file.iloc[prof_index][8]
@@ -41,18 +47,17 @@ for file_index in range(file_count):
         h_ind5 = input_file.iloc[prof_index][11]
         i_ind5 = input_file.iloc[prof_index][12] 
         cit_list = input_file.iloc[prof_index][13]
-        image_url = input_file.iloc[prof_index][14]
+        image_url = check_for_nan(input_file.iloc[prof_index][14])
         papers_url_list = input_file.iloc[prof_index][15]
         papers_title_list = input_file.iloc[prof_index][16]
 
-        if scholar_id == previous_scholar_id:
+        if scholar_id in scholar_ids:            
             if file_index not in redundant_entries:
-                redundant_entries[file_index] = 1
+                redundant_entries[file_index]=1
             else:
-                redundant_entries[file_index]+=1
-            continue
+                redundant_entries[file_index]+=1            
         else:
-            previous_scholar_id = scholar_id
+            scholar_ids.add(scholar_id)
             professor_data_to_write = (scholar_id, name, image_url, affiliation, email, homepage, topics_list, cit, h_ind, i_ind, cit5, h_ind5, i_ind5, cit_list, image_url, papers_url_list, papers_title_list)    
             write_prof_data_to_csv(output_file, professor_data_to_write)
 
