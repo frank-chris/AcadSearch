8b8def39cbf8377090a23831a22f99c1d88ff7ca
Nishikant Parmar <nishikantparmar123@gmail.com>
2021-04-07 16:25:57 +0530

    Added Flask ENV and made frontend

--- web_server/server.py
+++ web_server/server.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
+app.debug = True
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/', methods=['GET','POST'])
+def search():    
+    doc_ids = dict()  
+    try:
+        search_query = str(request.args['query']) 
+    except:
+        search_query = ''    
+    
+    doc_ids = {'0':1000, '1':2000}
+
+    return render_template('index.html',doc_ids=doc_ids)  
