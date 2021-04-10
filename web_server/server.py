83ae137c57011e8258965d009a4bb8dda49a70d7
Nishikant Parmar <nishikantparmar123@gmail.com>
2021-04-11 00:30:54 +0530

    Web server changes

--- web_server/server.py
+++ web_server/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.debug = True
 
 def get_doc_id_from_tf_idf(query):
-    return [i for i in range(100)]
+    return [i for i in range(10)]
 
 @app.route('/', methods=['GET','POST'])
 def search():  
@@ -17,11 +17,11 @@ def search():
         search_query = ''   
 
     if search_query == '':
-        return render_template('index.html',prof_data=prof_data,time_taken = round(time.time() - start_time, 4))  
+        return render_template('index.html',prof_data=prof_data,time_taken = round(time.time() - start_time, 5), search_query = search_query)  
     
     prof_ids = get_doc_id_from_tf_idf(search_query)
 
     for prof_id in prof_ids:    
         prof_data.append(read_prof_information(prof_id))
 
-    return render_template('index.html',prof_data=prof_data,time_taken = round(time.time() - start_time, 4)) 
+    return render_template('index.html',prof_data=prof_data,time_taken = round(time.time() - start_time, 5), search_query = search_query) 
